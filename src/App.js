50e0a5fa55a382c1929a2f859b75f3d9a8621f8b
BerkaySivri35 <1804107017@std.idu.edu.tr>
2022-11-06 19:14:34 +0300

    React Component Tasima

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './style.css'
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Main from './components/Main';
+import About from './components/About';
+import { Routes, Route } from 'react-router-dom';
+import Article from './components/Articles';
+import Educations from './components/Educations';
+import Notfound from './components/404';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class BlogApp extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    return (
+      <div>
+        
+        <Navbar />
+
+        <Routes>
+          <Route path="/main" element={<Main />}/>
+
+          <Route path="/about" element={<About />} />
+
+          <Route path="/articles" element={<Article />} />
+
+          <Route path="/educations" element={<Educations />} />
+
+          <Route path="*" element={<Notfound />} />
+
+        </Routes>
+
+        <Footer />
+      </div>
+    );
+  }
 }
 
-export default App;
+export default BlogApp;
